b2926b930195c7288ecf4240f0a67edcf4b5ad42
Kevin Loughead <kvnloughead@gmail.com>
2021-08-31 16:39:39 -0400

    add componentWillUnmount alert

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,20 @@ import Profile from './Profile';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { id: '' };
+    this.state = { id: '', isProfileVisible: false };
   }
 
   handleChange = (e) => {
     this.setState({
+      ...this.state,
       id: e.target.value,
+    })
+  }
+
+  showProfile = () => {
+    this.setState({
+      ...this.state,
+      isProfileVisible: !this.state.isProfileVisible,
     })
   }
 
@@ -21,7 +29,8 @@ class App extends React.Component {
           <label>Enter user id: </label>
           <input value={this.state.id} onChange={this.handleChange} />
         </form>
-        <Profile id={this.state.id} />
+        {this.state.isProfileVisible && (<Profile id={this.state.id} />)}
+        <button onClick={this.showProfile}>Show/hide profile</button>
       </div>
     );
   }
