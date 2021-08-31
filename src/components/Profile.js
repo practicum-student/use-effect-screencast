974d6b99e3292ade16797fe7e37d93f1ea914c50
Kevin Loughead <kvnloughead@gmail.com>
2021-08-31 15:12:56 -0400

    remove componentWillUpdate

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,47 +1,54 @@
 import React from 'react';
 
-class Profile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: '', email: '' };
-  }
+function Profile(props) {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');  
 
-  fetchUser = (id) => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({
-          ...this.state,
-          name: data.name,
-          email: data.email,
-        })
-      })
-      .catch((err) => console.log(err));
-  }
-
-  componentDidMount() {
-    this.fetchUser(this.props.id);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.id !== this.props.id) {
-      this.fetchUser(this.props.id);
-    }
-  }
-
-  componentWillUnmount() {
-    
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>{`Name: ${this.state.name || ''}`}</h2>
-        <p>{`Email: ${this.state.email || ''}`}</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>{`Name: ${name || ''}`}</h2>
+      <p>{`Email: ${email || ''}`}</p>
+    </div>
+  );
 }
+
+// class Profile extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { name: '', email: '' };
+//   }
+
+//   fetchUser = (id) => {
+//     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         this.setState({
+//           name: data.name,
+//           email: data.email,
+//         })
+//       })
+//       .catch((err) => console.log(err));
+//   }
+
+//   componentDidMount() {
+//     this.fetchUser(this.props.id);
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.id !== this.props.id) {  
+//       this.fetchUser(this.props.id);
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>{`Name: ${this.state.name || ''}`}</h2>
+//         <p>{`Email: ${this.state.email || ''}`}</p>
+//       </div>
+//     );
+//   }
+// }
 
 // Refactored Profile Component
 //
