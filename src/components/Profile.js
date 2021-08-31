9cb921350999e3628a0f5e00e1653996e940f2df
Kevin Loughead <kvnloughead@gmail.com>
2021-08-31 17:42:54 -0400

    final code

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,81 +1,71 @@
 import React from 'react';
 
-// function Profile(props) {
-//   const [name, setName] = React.useState('');
-//   const [email, setEmail] = React.useState('');  
-
-//   return (
-//     <div>
-//       <h2>{`Name: ${name || ''}`}</h2>
-//       <p>{`Email: ${email || ''}`}</p>
-//     </div>
-//   );
-// }
-
-class Profile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: '', email: '' };
-  }
-
-  fetchUser = (id) => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+function Profile(props) {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');  
+  
+  React.useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${props.id}`)
       .then((res) => res.json())
       .then((data) => {
-        this.setState({
-          name: data.name,
-          email: data.email,
-        })
+        setName(data.name);
+        setEmail(data.email);
       })
       .catch((err) => console.log(err));
-  }
 
-  componentDidMount() {
-    this.fetchUser(this.props.id);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.id !== this.props.id) {  
-      this.fetchUser(this.props.id);
+    return () => {
+      return console.log('Component has unmounted!');
     }
-  }
+  });
 
-  componentWillUnmount() {
-    alert('Bye!');
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>{`Name: ${this.state.name || ''}`}</h2>
-        <p>{`Email: ${this.state.email || ''}`}</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>{`Name: ${name || ''}`}</h2>
+      <p>{`Email: ${email || ''}`}</p>
+    </div>
+  );
 }
 
-// Refactored Profile Component
-//
-// function Profile(props) {
+// class Profile extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { name: '', email: '' };
+//   }
 
-//   const [name, setName] = React.useState('');
-//   const [email, setEmail] = React.useState('');
- 
-//   React.useEffect(() => {
-//     fetch(`https://jsonplaceholder.typicode.com/users/${props.id}`)
+//   fetchUser = (id) => {
+//     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
 //       .then((res) => res.json())
 //       .then((data) => {
-//         setName(data.name);
-//         setEmail(data.email);
+//         this.setState({
+//           name: data.name,
+//           email: data.email,
+//         });
 //       })
-//   });
+//       .catch((err) => console.log(err));
+//   }
 
-//   return (
-//     <div>
-//       <h2>{`Name: ${name}`}</h2>
-//       <p>{`Email: ${email}`}</p>
-//     </div>
-//   );
+//   componentDidMount() {
+//     this.fetchUser(this.props.id);
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.id !== this.props.id) {  
+//       this.fetchUser(this.props.id);
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     console.log('Compounded has unmounted!')
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>{`Name: ${this.state.name || ''}`}</h2>
+//         <p>{`Email: ${this.state.email || ''}`}</p>
+//       </div>
+//     );
+//   }
 // }
 
 export default Profile;
