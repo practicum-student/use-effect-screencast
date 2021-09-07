613f3e9828e55e3eb6d92fe2c2b0c1cedf9b0851
Kevin Loughead <kvnloughead@gmail.com>
2021-09-07 09:26:32 -0400

    reset to initial code

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,71 +1,45 @@
 import React from 'react';
 
-function Profile(props) {
-  const [name, setName] = React.useState('');
-  const [email, setEmail] = React.useState('');  
-  
-  React.useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${props.id}`)
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email: '' };
+  }
+
+  fetchUser = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => res.json())
       .then((data) => {
-        setName(data.name);
-        setEmail(data.email);
+        this.setState({
+          name: data.name,
+          email: data.email,
+        });
       })
       .catch((err) => console.log(err));
+  }
 
-    return () => {
-      return console.log('Component has unmounted!');
+  componentDidMount() {
+    this.fetchUser(this.props.id);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.id !== this.props.id) {  
+      this.fetchUser(this.props.id);
     }
-  });
+  }
 
-  return (
-    <div>
-      <h2>{`Name: ${name || ''}`}</h2>
-      <p>{`Email: ${email || ''}`}</p>
-    </div>
-  );
+  componentWillUnmount() {
+    console.log('Compounded has unmounted!')
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{`Name: ${this.state.name || ''}`}</h2>
+        <p>{`Email: ${this.state.email || ''}`}</p>
+      </div>
+    );
+  }
 }
-
-// class Profile extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { name: '', email: '' };
-//   }
-
-//   fetchUser = (id) => {
-//     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-//       .then((res) => res.json())
-//       .then((data) => {
-//         this.setState({
-//           name: data.name,
-//           email: data.email,
-//         });
-//       })
-//       .catch((err) => console.log(err));
-//   }
-
-//   componentDidMount() {
-//     this.fetchUser(this.props.id);
-//   }
-
-//   componentDidUpdate(prevProps) {
-//     if (prevProps.id !== this.props.id) {  
-//       this.fetchUser(this.props.id);
-//     }
-//   }
-
-//   componentWillUnmount() {
-//     console.log('Compounded has unmounted!')
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h2>{`Name: ${this.state.name || ''}`}</h2>
-//         <p>{`Email: ${this.state.email || ''}`}</p>
-//       </div>
-//     );
-//   }
-// }
 
 export default Profile;
